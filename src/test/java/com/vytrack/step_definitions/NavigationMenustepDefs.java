78041bd4b4a121df701dbb870c6a278ed8ad23de
Sevki <sevki.oksuz@gmail.com>
2022-02-10 13:45:30 +0100

    contact feature added

--- src/test/java/com/vytrack/step_definitions/NavigationMenustepDefs.java
+++ src/test/java/com/vytrack/step_definitions/NavigationMenustepDefs.java
@@ -1,7 +1,11 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.ContactsPage;
+import com.vytrack.pages.DashBoardPage;
+import com.vytrack.utilities.BrowserUtils;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class NavigationMenustepDefs {
     @When("the user navigates to Fleet, Vehicles")
@@ -38,19 +42,19 @@ public class NavigationMenustepDefs {
     }
 
 
-//    @When("the user navigates to {string} {string}")
-//    public void the_user_navigates_to(String tab, String module) {
-//        new DashboardPage().navigateToModule(tab,module);
-//    }
-//
-//
-//    @Then("default page number should be {int}")
-//    public void default_page_number_should_be(Integer expectedPageNum) {
-//        BrowserUtils.waitFor(3);
-//        ContactsPage contactsPage = new ContactsPage();
-//        Integer actualNumber =Integer.parseInt(contactsPage.pageNumber.getAttribute("value"));
-//
-//        Assert.assertEquals(expectedPageNum,actualNumber);
-//    }
+    @When("the user navigates to {string} {string}")
+    public void the_user_navigates_to(String tab, String module) {
+        new DashBoardPage().navigateToModule(tab,module);
+    }
+
+
+    @Then("default page number should be {int}")
+    public void default_page_number_should_be(Integer expectedPageNum) {
+        BrowserUtils.waitFor(3);
+        ContactsPage contactsPage = new ContactsPage();
+        Integer actualNumber =Integer.parseInt(contactsPage.pageNumber.getAttribute("value"));
+
+        Assert.assertEquals(expectedPageNum,actualNumber);
+    }
 
 }
